83e100f12223045dc67e8dbf436d33b599e074e0
Flavio Barci <flavio.barci@gmail.com>
2020-06-10 21:12:02 -0300

    Fix dmenu size

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx   = 2;        /* border pixel of windows */
 static const unsigned int snap       = 32;       /* snap pixel */
 static const int showbar             = 1;        /* 0 means no bar */
 static const int topbar              = 1;        /* 0 means bottom bar */
-static const char *fonts[]           = { "SF Mono Powerline:style=Medium:size=12:antialias=true:hinting=true:hintstyle=hintslight", "SauceCodePro Nerd Font Mono:pixelsize=12:antialias=true:hintstyle=hintslight" };
-static const char dmenufont[]        = "SF Mono Powerline:style=Medium:size=12:antialias=true:hinting=true:hintstyle=hintslight";
+static const char *fonts[]           = { "SF Mono Powerline:style=Medium:size=11:antialias=true:hinting=true:hintstyle=hintslight", "SauceCodePro Nerd Font Mono:pixelsize=12:antialias=true:hintstyle=hintslight" };
+static const char dmenufont[]        = "SF Mono Powerline:style=Medium:size=11:antialias=true:hinting=true:hintstyle=hintslight";
 static const char col_gray1[]        = "#030303";
 static const char col_gray2[]        = "#631dad";
 static const char col_gray3[]        = "#ffffff";
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-h", "28", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-h", "29", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
