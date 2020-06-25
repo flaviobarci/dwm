53d2b102fce40462b7f7e36f1b8615e411a07414
Flavio Barci <flavio.barci@gmail.com>
2020-06-25 17:05:29 -0300

    Add clip command

--- config.def.h
+++ config.def.h
@@ -62,6 +62,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-h", "28", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *clipcmd[]  = { "clipmenu",  "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-h", "28", NULL };
 
 /*First arg only serves to match against key in rules*/
 static const char *scratchpadcmd[] = {"t", "telegram-desktop", NULL}; 
@@ -69,7 +70,8 @@ static const char *scratchpadcmd[] = {"t", "telegram-desktop", NULL};
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd  } },
+  { MODKEY,                       XK_c,      spawn,          {.v = clipcmd  } },
 	{ MODKEY,                       XK_s,      togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
